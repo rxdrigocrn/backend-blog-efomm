b8a62bfd08d56efca3eaf894e7336922ecd6fc8f
rxdrigocrn <rodrigocirinocl@gmail.com>
2026-04-25 13:52:04 -0300

    fix: adjust import

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from '@prisma/client';
-import { ROLES_KEY } from 'src/roles.decorator';
+import { ROLES_KEY } from '../../roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
