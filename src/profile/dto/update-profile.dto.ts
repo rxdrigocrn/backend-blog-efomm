bca62dead4f33f809883811d80a4fa016b92d763
rxdrigocrn <rodrigocirinocl@gmail.com>
2026-04-25 13:55:27 -0300

    adjust packagejon

--- src/profile/dto/update-profile.dto.ts
+++ src/profile/dto/update-profile.dto.ts
@@ -2,4 +2,7 @@ import { PartialType } from '@nestjs/mapped-types'; // ou '@nestjs/swagger' se e
 import { CreateUserDto } from './create-profile.dto';
 
 // O PartialType faz com que todos os campos do CreateUserDto fiquem opcionais para o Update
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+ 
+  avatarUrl?: string;
+}
