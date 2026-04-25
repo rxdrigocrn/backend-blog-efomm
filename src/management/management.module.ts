448e791334cae3a94c3ea9d15416400b6bd7fc7b
rxdrigocrn <rodrigocirinocl@gmail.com>
2026-04-25 14:05:47 -0300

    fix modules

--- src/management/management.module.ts
+++ src/management/management.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ManagementService } from './management.service';
 import { ManagementController } from './management.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UploadService } from 'src/upload/upload.service';
+import { UploadModule } from 'src/upload/upload.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UploadModule],
   controllers: [ManagementController],
-  providers: [ManagementService],
+  providers: [ManagementService, UploadService],
 })
 export class ManagementModule {}
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,9 +3,10 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UploadService } from 'src/upload/upload.service';
+import { UploadModule } from 'src/upload/upload.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UploadModule],
   controllers: [PostsController],
   providers: [PostsService, UploadService],
 })
